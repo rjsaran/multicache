0df335d08b1d4f9369fe65e7e9fbcd5141efb0f4
Ramjeet Saran <saran.ramjeet@gmail.com>
2017-11-15 15:00:02 +0530

    using mget and mset with stats

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 
 const Cache = require('./lib/cache');
 
-let cache = new Cache('redis', {maxAge: 10 * 60 * 1000, reset: {interval: 10 * 1000}});
+let cache = Cache.stats.register(new Cache('redis', {maxAge: 10 * 60 * 1000, reset: {interval: 100 * 1000}}), 'mycache');
 
 let opts = {
   'columns': ['id', 'price']
@@ -12,12 +12,12 @@ let fetchData = function fetchData(ids, opts, cb) {
   let response = {};
   ids.forEach((id) => {
     response[id] = {
-      'price': 1000,
+      'price': 100 * id,
       'id': id
     };
   });
 
-  let t = Math.floor(Math.random() * 10 + 1);
+  let t = Math.floor(Math.random() * 5 + 1);
   console.log('Responding after Time: ', t, ' For ids: ', ids);
   setTimeout(() => {
     cb(null, response);
@@ -36,7 +36,7 @@ let fastFetchData = cache.wrap(function fastFetchData(ids, opts, cb) {
 fastFetchData([123, 124, 125], opts,  (err, res) => {
   console.log('First Response ', err, res);
   setTimeout(() => {
-    fastFetchData([123, 126, 125], opts, (err, res) => {
+    fastFetchData([123, 124, 125, 118, 12], opts, (err, res) => {
       console.log('Second Response ', err, res);
     });
   }, 2 * 1000);
